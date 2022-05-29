c80a56a25dfd5b8a601dcf5176fe236a030224eb
Kirill <59690988+kiryusha404@users.noreply.github.com>
2022-05-29 20:43:21 +0400

    Delete js directory

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function menu(){
-    var x = document.getElementById("myTopnav");
-
-    if(x.className === "topnav"){
-        x.className += " responsive";
-    } else{
-        x.className = "topnav";
-    }
-}
